f7c8f0649bcec8aac65f6fb9018bcb3376d05120
shlakoblokun22 <boenkodanil142@gmail.com>
2025-10-16 14:09:10 +0700

    Update Car.java

--- src/main/java/com/japancats/entity/Car.java
+++ src/main/java/com/japancats/entity/Car.java
@@ -17,7 +17,7 @@ public class Car {
     @Column(nullable = false)
     private String model;
 
-    @Column(name = "production_year")  // ИЗМЕНИТЕ ЗДЕСЬ
+    @Column(name = "production_year")  
     private Integer productionYear;
 
     private String imageUrl;
@@ -33,7 +33,7 @@ public class Car {
         this.productionYear = productionYear;
     }
 
-    // Геттеры и сеттеры
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
